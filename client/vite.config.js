104eb23dc17c7c400617b1c3b3bf1f4d439dd31a
AswathS2009 <saswath87@gmail.com>
2026-04-29 15:51:53 +0530

    Allow Railway frontend domain in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: [
+      "frontend-url-shortner-production.up.railway.app",
+      "localhost",
+      "127.0.0.1",
+    ],
+  },
+});
